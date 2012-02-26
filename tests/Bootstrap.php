787f2d52209a9ce1a119ac1b4aa3aa336347044e
Michael Contento <michaelcontento@gmail.com>
2012-02-26 17:24:47 +0100

    Syntaxerror fixed

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -4,5 +4,5 @@ if (!@include(__DIR__ . '/../vendor/.composer/autoload.php')) {
     echo "You must set up the project dependencies, run the following commands:"
         . "wget http://getcomposer.org/composer.phar"
         . " && php composer.phar install";
-    exit 1;
+    exit(1);
 }
